e0bfca1891ba5799d9b3ba65f7178f2fe4ec8d8b
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2018-12-03 21:18:20 -0800

    added list fature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag0', 'tag1', 'tag2']
     };
 
 
@@ -13,6 +14,9 @@ class Counter extends Component {
         return ( <div>
                         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                         <button className="btn btn-secondary btn-sm">Increment</button>
+                        <ul>
+                            {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+                        </ul>
         </div>
         );
     }
